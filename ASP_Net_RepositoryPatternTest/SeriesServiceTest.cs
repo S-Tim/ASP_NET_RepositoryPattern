1a7157c43a2eb0141cc2658493b4c5ce77d02f17
TSilhan <Tim.Silhan@uni-oldenburg.de>
2017-08-16 12:13:29 -0400

    Refactored unit test to test the service instead of the repository

--- ASP_Net_RepositoryPatternTest/SeriesServiceTest.cs
+++ ASP_Net_RepositoryPatternTest/SeriesServiceTest.cs
@@ -1,23 +1,23 @@
-﻿using ASP_Net_RepositoryPattern.DAL.Interfaces;
-using ASP_Net_RepositoryPattern.Models;
+﻿using ASP_Net_RepositoryPattern.Models;
+using ASP_Net_RepositoryPattern.Services;
 using ASP_Net_RepositoryPatternTest.Mocks.RepositoryMocks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ASP_Net_RepositoryPatternTest
 {
-    // TODO actually this should test a SeriesService that has an ISeriesRepository but I have not implemented the service yet. This is testing the MockRepository
     [TestClass]
     public class SeriesServiceTest
     {
-        private readonly ISeriesRepository _seriesRepository = new MockSeriesRepository();
+        private readonly SeriesService _seriesService = new SeriesService(new MockSeriesRepository());
 
         [TestMethod]
         public void TestAddSeriesAndGetById()
         {
             Series series = new Series { Title = "Game of Thrones", Episode = 1, Season = 2, Id = 123 };
 
-            _seriesRepository.Insert(series);
-            var retrieved = _seriesRepository.GetById(123);
+            _seriesService.InsertSeries(series);
+
+            var retrieved = _seriesService.GetSeries(123);
 
             Assert.AreEqual(series.Id, retrieved.Id);
             Assert.AreEqual(series.Title, retrieved.Title);
